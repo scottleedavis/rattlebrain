880064a6508ef04ed56629edef8ec52e0619b50d
scott davis <scottleedavis@gmail.com>
2024-12-03 10:41:47 -0800

    fixed main tests

--- tests/main_tests.rs
+++ tests/main_tests.rs
@@ -10,7 +10,7 @@ mod main_tests {
             .expect("Failed to execute process");
 
         let stdout = String::from_utf8(output.stdout).unwrap();
-        assert!(stdout.contains("Usage: rocket-league-replay-ai-analysis <command> [options]"));
+        assert!(stdout.contains("Usage: rl_replay_ai <command> [options]"));
     }
 
     #[test]
